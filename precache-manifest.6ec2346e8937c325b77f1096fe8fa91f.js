cd3ca2318d522f87b7e1b8f19df2b7e291ac87b4
craig <craigricetong@gmail.com>
2020-07-28 10:57:01 -0700

    Updates

---
+++ precache-manifest.6ec2346e8937c325b77f1096fe8fa91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2262c555fa6b812b916b1c1121855b15",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8461a7889eb6d899d01c",
+    "url": "./static/css/main.afd2950b.chunk.css"
+  },
+  {
+    "revision": "c0ae2baf0de77bbdba41",
+    "url": "./static/js/2.e4d752f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e4d752f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8461a7889eb6d899d01c",
+    "url": "./static/js/main.ab05d5e0.chunk.js"
+  },
+  {
+    "revision": "c2b788fc811a544b9f52",
+    "url": "./static/js/runtime-main.d4697a1f.js"
+  }
+]);
